86aa1c2bea64f73fbac3865a1e48717c87601b27
Daniel Göker <daniel.goker@gmail.com>
2018-12-11 15:20:45 +0100

    Convert to swift 4

--- Sources/MainThreadGuard.swift
+++ Sources/MainThreadGuard.swift
@@ -40,12 +40,12 @@ extension UIView {
         let originalMethod = class_getInstanceMethod(self, originalSelector)
         let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
 
-        let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+        let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod!), method_getTypeEncoding(swizzledMethod!))
 
         if didAddMethod {
-          class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+          class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod!), method_getTypeEncoding(originalMethod!))
         } else {
-          method_exchangeImplementations(originalMethod, swizzledMethod)
+          method_exchangeImplementations(originalMethod!, swizzledMethod!)
         }
       }
 
